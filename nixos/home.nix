1285d7716f84bf329363846b6390da44bc71eec1
William Lumme <80401792+wlumme@users.noreply.github.com>
2025-11-15 13:49:00 +0000

    feat[nixos]: Add multiple applications
    Brave
    Discord
    Spotify

--- nixos/home.nix
+++ nixos/home.nix
@@ -4,10 +4,14 @@
   home.username = "wlum";
   home.homeDirectory = "/home/wlum";
   home.packages = with pkgs; [
+    brave
+    discord
     gh
     git
     neofetch
     neovim
+    spotify
+    zoxide
   ];
 
   home.stateVersion = "25.05";
