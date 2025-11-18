4fe3fdf47db67f877e5254c0db60e07c48b5252f
William Lumme <80401792+wlumme@users.noreply.github.com>
2025-11-18 00:22:04 +0000

    feat[nixos]: Limit generations and schedule garbage collection

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -152,4 +152,23 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
+  # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/other-useful-tips#reducing-disk-usage
+
+  # Limit the number of generations to keep
+  boot.loader.systemd-boot.configurationLimit = 5;
+  # boot.loader.grub.configurationLimit = 5;
+
+  # Perform garbage collection weekly to maintain low disk usage
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
+  # Optimize storage
+  # You can also manually optimize the store via:
+  #    nix-store --optimise
+  # Refer to the following link for more details:
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
 }
