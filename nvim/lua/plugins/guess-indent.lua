13dc2e9f0d958e163b3cbc6fd36caa1b69b84b91
William Lumme <80401792+wlumme@users.noreply.github.com>
2025-11-30 22:38:45 +0000

    chore[nvim]: Reset config

--- nvim/lua/plugins/guess-indent.lua
+++
@@ -1,4 +0,0 @@
--- Indentation style detection
--- https://github.com/NMAC427/guess-indent.nvim
-
-return { "NMAC427/guess-indent.nvim", opts = {} }
--- nvim/lua/plugins/todo-comments.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/folke/todo-comments.nvim
--- Highlight and search for TODO comments
-
-return {
-    "folke/todo-comments.nvim",
-    event = "VimEnter",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { signs = false },
-}
